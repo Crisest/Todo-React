a929b1f855aae814600bf51066425d34e8333912
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-06-06 14:45:03 -0400

    Set up server and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
 // entry -> output
 const path = require('path')
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        //2 things
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+module.exports = () => {
+
+    const MiniCSSExtract = new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      });
+    return {
+        entry: './src/app.js',
+        output: {
+            //2 things
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+            ]
+        },
+        plugins: [
+            MiniCSSExtract
+        ],
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+    
+    }
 }
